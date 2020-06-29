1bbd6a6582f2325618143e6a6a164f314bbe3546
feksan <feksan90@gmail.com>
2020-06-29 22:14:52 +0200

    788

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -7,11 +7,6 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("asdaaaa!");
-            Console.WriteLine("asdaaaa!");
-            Console.WriteLine("asdaaaa!");
-            Console.WriteLine("asdaaaa!");
-            Console.WriteLine("asdaaaa!");
-          
         }
     }
 }
