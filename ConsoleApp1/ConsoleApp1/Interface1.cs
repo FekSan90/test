e15fa8475ff5904eb875e444b08bbaf5781ac159
feksan <feksan90@gmail.com>
2020-06-29 20:06:40 +0200

    BlAA bLA

---
+++ ConsoleApp1/ConsoleApp1/Interface1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    interface Interface1
+    {
+        void asd();
+    }
+}
