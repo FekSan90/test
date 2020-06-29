cdbe60b36b85fb781ff87659e3256c83a6ee9f96
feksan <feksan90@gmail.com>
2020-06-29 19:47:21 +0200

    new line

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hellosdsad World!");
+            Console.WriteLine("asd!");
         }
     }
 }
